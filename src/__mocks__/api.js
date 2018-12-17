23ff5d5898f1fbc843119bc8eae9048ed1406e3b
Jon Urry <jon@urry.me>
2018-12-17 15:33:49 +0000

    Got tests passing again
    Mock API wasnt returning an array of peep objects

--- src/__mocks__/api.js
+++ src/__mocks__/api.js
@@ -1,22 +1,24 @@
 export default class Api {
-  async get(url) {
-    return {
-      id: 3,
-      body: 'my first peep :)',
-      created_at: '2018-06-23T13:21:23.317Z',
-      updated_at: '2018-06-23T13:21:23.317Z',
-      user: {
-        id: 1,
-        handle: 'kay'
-      },
-      likes: [
-        {
-          user: {
-            id: 1,
-            handle: 'kay'
+  async getPeeps(url) {
+    return [
+      {
+        id: 3,
+        body: 'my first peep :)',
+        created_at: '2018-06-23T13:21:23.317Z',
+        updated_at: '2018-06-23T13:21:23.317Z',
+        user: {
+          id: 1,
+          handle: 'kay'
+        },
+        likes: [
+          {
+            user: {
+              id: 1,
+              handle: 'kay'
+            }
           }
-        }
-      ]
-    };
+        ]
+      }
+    ];
   }
 }
--- src/__tests__/Peeps.test.js
+++ src/__tests__/Peeps.test.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Peeps from '../Peeps';
 import Api from '../api.js';
+
 jest.mock('../api');
 
-it('renders without crashing', () => {
+it('renders without crashing', async () => {
   const api = new Api();
+  const peeps = await api.getPeeps();
   const div = document.createElement('div');
-  ReactDOM.render(<Peeps api={api} />, div);
+  ReactDOM.render(<Peeps api={api} peeps={peeps} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
