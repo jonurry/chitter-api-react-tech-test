db0201d865de4611eb7429981a80d4b9509b8ee0
Jon Urry <jon@urry.me>
2019-01-16 09:47:12 +0000

    Remove dependencies for Enzyme
    Tests are now using React Testing Library instead of Enzyme
    so remove Enzyme and tidy up.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,2 @@
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
